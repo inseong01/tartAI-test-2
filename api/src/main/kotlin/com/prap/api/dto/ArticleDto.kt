7ff847f32d22ef1f8cd92a5f109c98fb999ca9e6
inseong01 <insung940@naver.com>
2026-02-13 19:42:08 +0900

    fix(api): ArticleDto Jackson 역직렬화 문제
    - 각 객체 속성에 기본값 부여

--- api/src/main/kotlin/com/prap/api/dto/ArticleDto.kt
+++ api/src/main/kotlin/com/prap/api/dto/ArticleDto.kt
@@ -5,36 +5,36 @@ import jakarta.validation.constraints.NotBlank
 
 
 data class CategoryDto(
-    val name: String,
-    val url: String
+    val name: String = "",
+    val url: String = ""
 )
 
 data class AuthorDto(
-    val name: String,
-    val url: String
+    val name: String = "",
+    val url: String = ""
 )
 
 data class ArticleDto(
     @field:NotBlank(message = "아이디는 필수입니다")
-    val id: String,
+    val id: String = "",
 
     @field:NotBlank(message = "제목은 필수입니다")
-    val title: String,
+    val title: String = "",
 
     @field:NotBlank(message = "URL은 필수입니다")
-    val url: String,
+    val url: String = "",
 
     @field:NotBlank(message = "섬네일 URL은 필수입니다")
-    val thumbnail: String,
+    val thumbnail: String = "",
 
-    val category: CategoryDto,
+    val category: CategoryDto = CategoryDto("", ""),
 
     @field:NotBlank(message = "발췌는 필수입니다")
-    val excerpt: String,
+    val excerpt: String = "",
 
-    val author: AuthorDto,
+    val author: AuthorDto = AuthorDto("", ""),
 
     @field:NotBlank(message = "발행일은 필수입니다")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
-    val published_date: String,
+    val published_date: String = ""
 )
