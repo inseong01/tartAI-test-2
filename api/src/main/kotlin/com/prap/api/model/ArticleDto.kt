194c5fd066ab7f25d32d245d4103ceac696019c0
inseong01 <insung940@naver.com>
2026-02-13 14:58:41 +0900

    feat(api): 기사 DTO 구현
    - 스크래퍼 반환 기사 DTO 생성
    - 카테고리, 저자 합친 기사 DTO
    - 필수 속성 지정

---
+++ api/src/main/kotlin/com/prap/api/model/ArticleDto.kt
@@ -0,0 +1,39 @@
+package com.prap.api.model
+import com.fasterxml.jackson.annotation.JsonFormat
+import jakarta.validation.constraints.NotBlank
+
+
+data class CategoryDto (
+    val name: String,
+    val url: String
+)
+
+data class AuthorDto (
+    val name: String,
+    val url: String
+)
+
+data class ArticleDto(
+    @field:NotBlank(message = "아이디는 필수입니다")
+    val id: String,
+
+    @field:NotBlank(message = "제목은 필수입니다")
+    val title: String,
+
+    @field:NotBlank(message = "URL은 필수입니다")
+    val url: String,
+
+    @field:NotBlank(message = "섬네일 URL은 필수입니다")
+    val thumbnail: String,
+
+    val category: CategoryDto,
+
+    @field:NotBlank(message = "발췌는 필수입니다")
+    val excerpt: String,
+
+    val author: AuthorDto,
+
+    @field:NotBlank(message = "발행일은 필수입니다")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    val published_date: String,
+)
