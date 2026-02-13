86394075f8daface82f369281037aea315ab71b5
inseong01 <insung940@naver.com>
2026-02-13 18:22:31 +0900

    refactor(api): 파이썬 테스트 실행기 기능 수정
    - exception 매개변수 타입 수정
      - Exception -> RuntimeException

--- api/src/test/kotlin/com/prap/api/python/FakePythonRunner.kt
+++ api/src/test/kotlin/com/prap/api/python/FakePythonRunner.kt
@@ -2,7 +2,7 @@ package com.prap.api.python
 
 class FakePythonRunner(
     private val result: String,
-    private val exception: Exception? = null
+    private val exception: RuntimeException? = null
 ) : PythonRunner() {
 
     override fun run(): String {
