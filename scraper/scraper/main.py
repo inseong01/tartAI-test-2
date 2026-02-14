4022de2eaf10901e2a430d74cc547a8ed0073523
inseong01 <insung940@naver.com>
2026-02-14 12:59:14 +0900

    refactor(scraper): 스크래퍼 기능 수정
    - 테스트용 함수 제외 대신 주석 처리
    - 데이터 반환하지 않고 출력되도록 메인 기능 수정

--- scraper/scraper/main.py
+++ scraper/scraper/main.py
@@ -1,6 +1,10 @@
 from .url_utils import fetch_url, get_articles
 from .loop_articles import loop_processing_articles
 
+# from .mock.loader import load_mock_data
+
+import json
+
 
 def scrape_prap_articles():
     url = "https://prap.kr/?cat=5"
@@ -9,7 +13,10 @@ def scrape_prap_articles():
     articles = get_articles(response.text)
     data = loop_processing_articles(articles)
 
-    return data
+    # 테스트 용도
+    # data = load_mock_data()
+
+    print(json.dumps(data, ensure_ascii=False), flush=True)
 
 
 scrape_prap_articles()
